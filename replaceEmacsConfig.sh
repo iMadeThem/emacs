e7d05ca96efa17e7cb98508ff5ae000425f4536f
Leon He <leon.he@gree.net>
2013-06-20 13:23:40 +0900

    [EMACS] Initial commit.

---
+++ replaceEmacsConfig.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+NOW=$(date +"%Y%m%d")
+DOTEMACS=~/.emacs
+DOTEMACSDOTD=~/.emacs.d
+
+echo "This will replace the emacs setting under you home folder: .emacs, .emacs.d/ !"
+echo "Press Y to continue, other to abort: "
+
+
+read input
+
+
+if [ $input = "Y" ] ; then
+    echo "Replacing..."
+    if [ -f "$DOTEMACS" ]; then
+	mv ~/.emacs ~/.emacs.bak.$NOW
+    fi
+    if [ -d "$DOTEMACSDOTD" ]; then
+	mv ~/.emacs.d ~/.emacs.d.bak.$NOW
+	mkdir ~/.emacs.d
+    fi
+    cp ./dotemacs.txt ~/.emacs
+    cp -R ./dotemacs.d/* ~/.emacs.d/
+    echo "Replace finishd."
+else
+    echo "Not replaced."
+    exit
+fi
